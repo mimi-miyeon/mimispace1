812f9f1d1a1d7e61681f1a704a2f08cf150633ea
Miyeon Mimi Yang <miyeon0403@hotmail.com>
2026-05-04 19:17:22 +0900

    delete nav

--- js/init.js
+++ js/init.js
@@ -3,8 +3,6 @@ import { fetchWeatherData } from './weather.js';
 import { drawTime } from './time.js';
 import { fetchProjectList } from './projectList.js';
 import { f_header_img } from './headerImg.js';
-import { f_indicator } from './indicator.js';
-
 const userLanguage = (navigator.languages !== undefined) ? navigator.languages[0] : navigator.language;
 let lang = (userLanguage !== 'en') ? 'ko' : 'en';
 sessionStorage.setItem('lang', lang);
@@ -22,7 +20,6 @@ async function fetchHTML() {
     fetchProjectList(lang);
     f_header_img();
     fetchWeatherData();
-    f_indicator();
     f_addEventHandler();
   } catch (error) {
     console.error('Error fetching page:', error);
